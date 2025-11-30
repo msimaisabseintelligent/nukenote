813e2e9aebb1fae5ac231527dcf50406b9e0ffb4
msimaisabseintelligent <151725079+msimaisabseintelligent@users.noreply.github.com>
2025-12-01 03:01:12 +0530

    Add files via upload

---
+++ copy-of-nukenote2222/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import { AuthProvider } from "./contexts/AuthContext";
+
+const rootElement = document.getElementById("root");
+if (rootElement) {
+  const root = createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <AuthProvider>
+        <App />
+      </AuthProvider>
+    </React.StrictMode>
+  );
+}
